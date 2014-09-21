ca8955bb033e07d0dfc94642fb107231f30c9984
the1sky <dumingtan@gmail.com>
2014-09-22 02:22:18 +0800

    # update

--- installChrome.sh
+++ installChrome.sh
@@ -4,24 +4,23 @@ if [ $? -eq 0 ]; then
     wget http://chrome.richardlloyd.org.uk/install_chrome.sh
     chmod +x install_chrome.sh
     sh install_chrome.sh
-fi
-
-
-cat /etc/issue | grep Ubuntu
-if [ $? -eq 0 ]; then
-    base_url='http://pagetimeline.duapp.com/browser/ubuntu/'
-    prefix='google-chrome-stable_current'
-    suffix='.deb'
-    cd /tmp
-    arch=`uname -m`
-    if [ $arch == 'x86_64' ]; then
-        arch='_amd64'
-    else
-        arch='_i386'
+else
+    cat /etc/issue | grep Ubuntu
+    if [ $? -eq 0 ]; then
+        base_url='http://115.28.244.165//browser/ubuntu/'
+        prefix='google-chrome-stable_current'
+        suffix='.deb'
+        cd /tmp
+        arch=`uname -m`
+        if [ $arch == 'x86_64' ]; then
+            arch='_amd64'
+        else
+            arch='_i386'
+        fi
+        chrome_url=${base_url}${prefix}${arch}${suffix}
+        wget $chrome_url
+        chmod u+x ${prefix}${arch}${suffix}
+        sudo dpkg -i google-chrome*
+        sudo apt-get -y -f install
     fi
-    chrome_url=${base_url}${prefix}${arch}${suffix}
-    wget $chrome_url
-    chmod u+x ${prefix}${arch}${suffix}
-    sudo dpkg -i google-chrome*
-    sudo apt-get -y -f install
 fi
